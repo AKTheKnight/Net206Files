cb2004553e8e3dad5f78b8dc8885b24c2c22cf80
Alex Knight <alex@aktheknight.co.uk>
2018-04-29 19:08:39 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo gethostname() . ”<br>\n";
+$UniID = "10567939";
+$IAM = “alexander@3475-4647-3148";
+echo hash('md5',$UniID) . "<br>\n";
+echo hash('md5',$IAM) . "<br>\n";
+$i = 1;
+$j = 0;
+$fsum = 0.0;
+while ($i <= 100000):
+$j = rand();
+#
+echo $j . "<br>\n";
+$fsum = $fsum + $j*$j/getrandmax()/getrandmax();
+$i++;
+endwhile;
+echo "\n" . $fsum/$i . "<br>\n";
+$time = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
+echo "Time: {$time}";
+?>
